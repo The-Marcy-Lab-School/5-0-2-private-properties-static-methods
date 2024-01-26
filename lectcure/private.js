01b13d67cab299f04a6fc2486d4b809731e849df
gonzalorportfolio <gromeroeducation@gmail.com>
2024-01-25 23:09:58 -0500

    first draft

---
+++ lectcure/private.js
@@ -0,0 +1,31 @@
+// Good but could be better!
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.password = null;
+  }
+
+  setPassword(newPassword) {
+    this.password = newPassword;
+  }
+
+  validatePassword(passwordToCheck) {
+    if (!this.password) {
+      console.log('No password set.');
+      return false;
+    }
+    if (passwordToCheck === this.password) {
+      console.log('It matches!');
+      return true;
+    }
+    console.log('Wrong password!');
+    return false;
+  }
+}
+
+const ben = new User('ben', 'ben@mail.com');
+ben.validatePassword('1234'); // No password set.
+ben.setPassword('1234');
+ben.validatePassword('1234'); // It Matches!
+
